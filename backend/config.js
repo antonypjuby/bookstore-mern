8d7c670659a88544200421ee367f83f6c8a7f368
Antony <tonypjuby@gmail.com>
2024-07-22 21:21:47 +0530

    mongo config

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL='mongodb+srv://user:passwordpassword@bookstore.2yfepai.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore'
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express"
-import { PORT } from "./config.js"
+import { PORT , mongoDBURL} from "./config.js"
+import mongoose from "mongoose"
 const app=express()
 app.get('/',(request,response)=>{
 console.log(request)
 return response.status(234).send('Welocome to Mern Stack tutorial')
 })
-app.listen(PORT,()=>{
-    console.log(`app listeing at port: ${PORT}`);
-});
 
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('app connected to database');
+        app.listen(PORT,()=>{
+            console.log(`app listeing at port: ${PORT}`);
+        });
+    })
+    .catch((error)=>{console.log(error)});
