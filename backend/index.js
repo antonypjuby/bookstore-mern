9e82885e3b9c42d0a33947b88375dd5490b0bfc6
Antony <tonypjuby@gmail.com>
2024-07-24 01:32:06 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 import express from "express"
+import cors from "cors";
 import { PORT , mongoDBURL} from "./config.js"
 import mongoose from "mongoose"
 import booksRoute from "./routes/booksRoute.js"
 const app=express()
 app.use(express.json());
-app.use(cors({
-    origin:'http://localhost:3000',
-    methods:['GET','POST','PUT','DELETE'],
-    allowedHeaders:['Content-Type'],
-})
-);
+  app.use(cors({
+      origin:'http://localhost:5173',
+      methods:['GET','POST','PUT','DELETE'],
+      allowedHeaders:['Content-Type'],
+  })
+  );
 app.get('/',(request,response)=>{
 console.log(request)
 return response.status(234).send('Welocome to Mern Stack tutorial')
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import Showbook from './pages/Showbook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<CreateBook/>}/>
+      <Route path='/books/details/:id' element={<Showbook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+
+
+    </Routes>
   )
 }
 
