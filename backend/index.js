7fc11099cc274af970438561fe3fa65ea8f542a5
Antony <tonypjuby@gmail.com>
2024-07-23 10:01:41 +0530

    react

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ import mongoose from "mongoose"
 import booksRoute from "./routes/booksRoute.js"
 const app=express()
 app.use(express.json());
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type'],
+})
+);
 app.get('/',(request,response)=>{
 console.log(request)
 return response.status(234).send('Welocome to Mern Stack tutorial')
